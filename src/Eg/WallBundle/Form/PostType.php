31d480c7b376964112fd47ca3ab7e07ef9321b9f
Zagonine <hello@zagonine.io>
2016-05-15 15:50:48 +0200

    Generate CRUD action of the Entity Post

---
+++ src/Eg/WallBundle/Form/PostType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Eg\WallBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('pseudo')
+            ->add('content')
+            ->add('datePosted', 'datetime')
+            ->add('categories')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Eg\WallBundle\Entity\Post'
+        ));
+    }
+}
